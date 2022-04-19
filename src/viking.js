ca90ea634527c5feb433206be49a23be1c6899f5
Klaus Haugnaess <klaushaugness@gmail.com>
2022-04-19 18:15:04 +0200

    done!

--- src/viking.js
+++ src/viking.js
@@ -83,7 +83,13 @@ class War {
  this.vikingArmy.pop(randomViking);
  }
  showStatus(){
-
+    if(saxonArmy.length === 0){
+      return "Vikings have won the war of the century!"
+    }else if(vikingArmy.length === 0){
+      return "Saxons have fought for their lives and survived another day..."
+    }else{
+      return "Vikings and Saxons are still in the thick of battle."
+    }
  }
 
 }
